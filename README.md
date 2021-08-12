0a7cdbaa32ced16cd8802d7d45e1191d49e32f5b
Artem <luardev@gmail.com>
2021-08-12 09:34:30 +0300

    Webcam support, video output, and capturing resolution specification added; known bugs are fixed

--- README.md
+++ README.md
@@ -1,12 +1,11 @@
 # EuresysCamCapture
 A python app for camera capturing from a Euresys grabber (Coaxlink Quad G3): live view with frames recording based on OpenCV.
 
-Author: Artem Lutov &lt;&#108;u&#97;&commat;&#108;ut&#97;n.ch&gt;  
+Author: Artem Lutov &lt;&#108;u&#97;&commat;&#108;ut&#97;n.ch&gt;, Valentyna Pryhodiuk &lt;v&#112;ryhodiuk&commat;lumais&#46;&#99;om&gt;  
 License: [Apache License, Version 2](www.apache.org/licenses/LICENSE-2.0.html)  
-Organizations: [UNIFR](https://www.unifr.ch), [Lutov Analytics](https://lutan.ch/)
+Organizations: [UNIFR](https://www.unifr.ch), [Lutov Analytics](https://lutan.ch/), [LUMAIS](http://lumais.com)
 
 ## Requirements
-
 1. Install [Euresys Grabber drivers for Coaxlink](https://www.euresys.com/en/Support/Download-area?Series=105d06c5-6ad9-42ff-b7ce-622585ce607f) >= v13
 2. Prepare Environment on Linux:
 ```sh
@@ -18,34 +17,56 @@ export EURESYS_DEFAULT_GENTL_PRODUCER=coaxlink
 ```
 3. Install Python bindings for eGrabber:
 ```sh
-$ python -m pip install /opt/euresys/egrabber/python/egrabber-13.0.1.32-py2.py3-none-any.whl
+$ python3 -m pip install /opt/euresys/egrabber/python/egrabber-13.0.1.32-py2.py3-none-any.whl
 ```
 
 ## Usage
 ```sh
-$ ./camcapture.py -h
-usage: camcapture.py [-h] [-f FPS] [-i {png,tiff}] [-n NFRAMES] [-o OUTP_DIR]
+./camcapture.py -h
+usage: camcapture.py [-h] [-f FPS] [-i {png,tiff,bmp,mp4}] [-n NFRAMES]
+                     [-o OUTP_DIR] [-c CAMERA] [-w WND_WIDTH] [-r RESOLUTION]
 
 Document Taxonomy Builder.
 
 optional arguments:
   -h, --help            show this help message and exit
   -f FPS, --fps FPS     Capturing speed (Frames Per Second) (default: 5)
-  -i {png,tiff}, --img-format {png,tiff}
+  -i {png,tiff,bmp,mp4}, --img-format {png,tiff,bmp,mp4}
                         Format of the capturing frames (default: png)
   -n NFRAMES, --no-gui-frames-number NFRAMES
                         The number of frames to be captured, being started
                         without any Graphical User Interface (default: 0)
   -o OUTP_DIR, --outp-dir OUTP_DIR
                         Output directory for the captured images (default:
-                        imgs)
+                        res)
+  -c CAMERA, --camera CAMERA
+                        The camera index to start the capturing (default: 0)
+  -w WND_WIDTH, --wnd-width WND_WIDTH
+                        Initial wide of the GUI window in pixels (default:
+                        1600)
+  -r RESOLUTION, --resolution RESOLUTION
+                        Show ("x") or set ("<W>x<H>") camera resolution in the
+                        format: <WIDTH>x<HEIGHT>, e.g., 800x600 (default:
+                        None)
 ```
+
+### GUI Mode
+
+To start/stop the **recording** (video stream of image files saving), press `r` or `spacebar` keys.
+
+It is possible to select the capturing **ROI** using the left mouse button, which is possible even during the recording process, automatically creating the respective output stream. The ROI is selected by pressing the left mouse button, moving the mouse cursor to form the rectangular ROI area, and releasing the left mouse button to complete the ROI selection. A ROI is reset by the left mouse click on a single pixel (without the mouse move).
 
 ## Examples
 Run in the GUI mode (live view), using default settings:
 ```sh
 ./camcapture.py
 ```
+
+Run in the GUI mode (live view), capturing from camera #`0` in the resolution `4096x3072` and `4` frames per second rate, saving the captured frames as a video stream in the `mp4` format in the directory `./video` (the video is suffixed with the prefix that corresponds to the starting frame):
+```sh
+./camcapture.py -c 0 -r 4096x3072 -f 4 -i mp4 -o video
+```
+
 Run in the console mode, record 100 frames in TIFF format (near real-time encoding, but consumes 3x more space than PNG) on 10 FPS to the `otiff/` directory:
 ```sh
 ./camcapture.py -n 100 -i tiff -f 10 -o otiff
--- camcapture.py
+++ camcapture.py
@@ -2,8 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 :Description: Camera capturing app for a Eursys Coaxlink camera grabber
-
-:Authors: (c) Artem Lutov <lua@lutan.ch>
+:Authors: (c) Artem Lutov <lua@lutan.ch>, Valentyna Pryhodiuk <vpryhodiuk@lumais.com>
 :Date: 2020-10-21
 """
 import os
@@ -16,59 +15,132 @@ from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 from enum import Enum, auto
 from egrabber import *
 
+# Drawing rect to define the roiRect
+drawingRect = None
+
+# ROI coordinates (x0, y0), (x1, y1) of the top left and bottom right corners
+roiRect = None
+
+# Video  output variables
+fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+initVideo = None
+vidout = None
+
 
 class ImageFormat(Enum):
 	"""Image formats to store the capturing frames"""
 	PNG = auto()
 	TIFF = auto()
+	BMP = auto()
+	MP4 = auto()
 
 	def __str__(self):
 		return self.name.lower()
+
 
 def rgb8_to_ndarray(rgb, w, h):
 	data = cast(rgb.get_address(), POINTER(c_ubyte * rgb.get_buffer_size())).contents
 	c = 3  # 3 channels
 	return np.frombuffer(data, count=rgb.get_buffer_size(), dtype=np.uint8).reshape((h,w,c))
 
+
 async def recordFrame(rgb, outdir, framenum, imgfmt):
 	"""Save a frame to the disk asynchronously
-
-	rgb: Buffer  - egrabber buffer in the required RGB format
+	
+	rgb: egrabber.ConvertedBuffer | numpy.ndarray  - egrabber buffer in the required RGB format or OpenCV image
 	outdir: str  - output directory
 	framenum: uint  - frame number
 	imgfmt: str  - image format of the output frames
 	"""
-	rgb.save_to_disk(os.path.join(outdir, 'frame.{:03}.{}'.format(framenum, imgfmt)))  # tiff
+	vid = imgfmt == str(ImageFormat.MP4)  # Video output
+	if isinstance(rgb, egrabber.ConvertedBuffer):
+		assert not (roiRect or vid), 'rgb buffer shout not be used with ROI or in video mode'
+		rgb.save_to_disk(os.path.join(outdir, 'frame-{:03}.{}'.format(framenum, imgfmt)))  # tiff
+	else:
+		if roiRect:
+			rgb = rgb[roiRect[0][1]:roiRect[1][1], roiRect[0][0]:roiRect[1][0]]
+		if vid:
+			# print(' [saving video frame: ({}, {}) / {}]'.format(rgb.shape[1], rgb.shape[0], roiRect))
+			vidout.write(rgb)
+		else:
+			cv2.imwrite(os.path.join(outdir, 'frame-{:03}.{}'.format(framenum, imgfmt)), rgb)
 
-#def setRoi():
+
+def setRoi(event, x, y, flags, params):
+	"""Mouse callback to set ROI
+	
+	event  - mouse event
+	x: int  - x coordinate
+	x: int  - y coordinate
+	flags  - additional flags
+	params - extra parameters
+	"""
+	global drawingRect, roiRect, initVideo
+	if event == cv2.EVENT_LBUTTONDOWN:
+		drawingRect = [(x, y), (x, y)]
+		# print('\nStrting drawingRect: ', drawingRect)
+	elif event == cv2.EVENT_LBUTTONUP:
+		pTL = (min(drawingRect[0][0], drawingRect[1][0]), min(drawingRect[0][1], drawingRect[1][1]))
+		pBR = (max(drawingRect[0][0], drawingRect[1][0]), max(drawingRect[0][1], drawingRect[1][1]))
+		drawingRect = None
+		# Set the roiRect or reset it
+		if pTL != pBR or roiRect:
+			initVideo = True  # Init video output
+		if pTL != pBR:
+			# Adjust to X px padding
+			pxBlock = 8
+			dx = (pBR[0] - pTL[0]) % pxBlock
+			if dx:
+				dx = pxBlock - dx
+			dy = (pBR[1] - pTL[1]) % pxBlock
+			if dy:
+				dy = pxBlock - dy
+			roiRect = (pTL, (pBR[0] + dx, pBR[1] + dy))
+			# print('\nROI size is corrected by ({}, {}): ({}, {})'.format(dx, dy, roiRect[1][0]-roiRect[0][0], roiRect[1][1]-roiRect[0][1]))
+		else:
+			roiRect = None			
+
+	if event == cv2.EVENT_RBUTTONUP:
+		drawingRect = None
+		roiRect = None
+
+	if event == cv2.EVENT_MOUSEMOVE and drawingRect:
+		drawingRect[1] = (x, y)
+
 
 # , vidout
-async def loop(grabber, nframes, tpf, outdir, imgfmt):
+async def loop(grabber, nframes, tpf, outdir, imgfmt, winw):
 	"""Capturing loop
-
+	
     grabber  - camera grabber object
     nframes: int  - the number of frames to be captured in the non-GUI mode;
-    tpf: float  - min time per frame in milli seconds
+    tpf: float  - min time per frame in seconds
     outdir: str  - output directory for the resulting frames
     imgfmt: str  - image format of the output frames
+    winw: int, >= 1  - Initial wide of the GUI window in pixels
     """
+	global initVideo, vidout
 	frameCount = 0
 	record = nframes > 0  # Whether to record the capturing frame as images
 	grabber.start()
+
 	if not nframes:
 		wTitle = 'Press ESC to exit, SPACEBAR to record'
-		w0 = 1600
+		w0 = winw
 		cv2.namedWindow(wTitle, cv2.WINDOW_NORMAL)
 		rfont = 0
-		#cv2.setMouseCallback(wTitle, setRoi)
+		cv2.setMouseCallback(wTitle, setRoi)
 	else:
 		print('Recorded frames:', end='')
+	videoOutp = imgfmt == str(ImageFormat.MP4)
+	initVideo = videoOutp  # Init video output
 	while True:
 		start = time.perf_counter()
 		# timeout in milliseconds
 		with Buffer(grabber, timeout=1000) as buffer:
 			rgb = buffer.convert('BGR8')  # OpenCV uses BGR colors
 			if not nframes:
+				# Note: initVideo is defined in mouse callback, where imgfmt is unknown
 				if not rfont:
 					w = buffer.get_info(BUFFER_INFO_WIDTH, INFO_DATATYPE_SIZET)
 					h = buffer.get_info(BUFFER_INFO_HEIGHT, INFO_DATATYPE_SIZET)
@@ -89,11 +161,26 @@ async def loop(grabber, nframes, tpf, outdir, imgfmt):
 					if record:
 						print('Recorded frames:', end='')
 
-				img = rgb8_to_ndarray(rgb, w, h)
+				img = rgb8_to_ndarray(rgb, w, h)  # Note: it fetches the memory rather than making a copy
+				if drawingRect or record:
+					img = img.copy()  # Copy image to not draw in over the original frame
+				if roiRect or drawingRect:
+					rc = drawingRect if drawingRect else roiRect
+					img = cv2.rectangle(img, rc[0], rc[1], (0, 255, 0))
 				if record:
+					if initVideo and videoOutp:
+						if roiRect:
+							vw = roiRect[1][0] - roiRect[0][0]
+							vh = roiRect[1][1] - roiRect[0][1]
+						else:
+							vw = w
+							vh = h
+						print(' [initializing video as: ({}, {})]'.format(vw, vh))
+						vidout = cv2.VideoWriter(os.path.join(outdir, 'video-{:03}.{}'.format(frameCount+1, imgfmt)), fourcc, 1./tpf, (vw,vh))
+						initVideo = False
 					_, _, w0, h0 = cv2.getWindowImageRect(wTitle)
-					img = cv2.resize(img, (w0, h0))
-					rfont = 1  # max(1, w / w0)
+					# img = cv2.resize(img, (w0, h0))
+					rfont = max(1, w / w0)
 					cv2.putText(img,'R',
 						(20, 20 + int(24 * rfont)),  # bottomLeftCornerOfText
 						cv2.FONT_HERSHEY_SIMPLEX,
@@ -107,27 +194,113 @@ async def loop(grabber, nframes, tpf, outdir, imgfmt):
 			# Ensure that fps does not exceed teh required value
 			dt = time.perf_counter() - start
 			if dt < tpf:
-				time.sleep(dt / 1000)
-			# Note: TIF images take 3.5x more space than PNG, but required much less CPU
+				time.sleep(tpf - dt)
+			# Note: TIFА images take 3.5x more space than PNG, but required much less CPU
 			if record:
-				await recordFrame(rgb, outdir, frameCount, imgfmt)
+				vid = imgfmt == str(ImageFormat.MP4)  # Video output
+				img = None
+				if roiRect or imgfmt == str(ImageFormat.MP4):
+					img = rgb8_to_ndarray(rgb, w, h)
+				await recordFrame(img if img is not None else rgb, outdir, frameCount, imgfmt)
 				print(' ', frameCount, end='', sep='', flush=True)
-			# img = rgb8_to_ndarray(rgb, w, h)
-			# vidout.write(img)
 	print()  # Ensure newline after the frames output
 
-def run(grabber, nframes, fps, outdir, imgfmt):
-	grabber.realloc_buffers(8)  # 3
-	# w = grabber.get_width()
-	# h = grabber.get_height()
-	# fourcc = cv2.VideoWriter_fourcc(*'DIVX')
-	# outdir = os.path.splitext(__file__)[0] + '.output'
-	if not os.path.isdir(outdir):
-		os.makedirs(outdir)
-	# out = cv2.VideoWriter(os.path.join(outdir, 'output.avi'), fourcc, fps, (w,  h))
-	asyncio.run(loop(grabber, nframes, 1000 / fps, outdir, imgfmt))
+
+async def wloop(vid, nframes, tpf, outdir, imgfmt, winw):
+	"""Capturing loop for webcamera
+	
+    vid  - OpenCV video device
+    nframes: int  - the number of frames to be captured in the non-GUI mode;
+    tpf: float  - min time per frame in seconds
+    outdir: str  - output directory for the resulting frames
+    imgfmt: str  - image format of the output frames
+    winw: int, >= 1  - Initial wide of the GUI window in pixels
+    """
+	global initVideo, vidout
+	wTitle = 'Press ESC to exit, SPACEBAR to record'
+	frameCount = 0
+	record = nframes > 0  # Whether to record the capturing frame as images
+
 	if not nframes:
-		cv2.destroyAllWindows()
+		wTitle = 'Press ESC to exit, SPACEBAR to record'
+		w0 = winw
+		cv2.namedWindow(wTitle, cv2.WINDOW_NORMAL)
+		rfont = 0
+		cv2.setMouseCallback(wTitle, setRoi)
+	else:
+		print('Recorded frames:', end='')
+	videoOutp = imgfmt == str(ImageFormat.MP4)
+	initVideo = videoOutp  # Init video output
+	while True:
+		start = time.perf_counter()
+		# timeout in milliseconds
+		ret, frame = vid.read()
+		img = frame.copy()
+		if not nframes:
+			if not rfont:
+				h, w = frame.shape[:2]
+				rfont = w / w0
+				cv2.resizeWindow(wTitle, w0, int(h / rfont))
+			#img = cv2.resize(frame, (w0, int(h / rfont)))
+			# cv2.imshow(wTitle, img)
+
+			key = cv2.waitKey(1) & 0xFF
+			# Quit: escape, e or q
+			if key in (27, ord('e'), ord('q')):
+				break
+			## Pause: spacebar or p
+			#elif key in (32, ord('p')):
+			#	cv2.waitKey(-1)
+			# Record: enter, spacebar or r
+			elif key in (10, 32, ord('r')):
+				record = not record
+				print(('' if record else '\n') + 'Recording: ',
+					os.path.normpath(outdir) + '/' if record else 'OFF')
+				if record:
+					print('Recorded frames:', end='')
+				elif vidout is not None:
+					vidout.release()
+			# img = frame.copy()
+			if roiRect or drawingRect:
+				rc = drawingRect if drawingRect else roiRect
+				img = cv2.rectangle(img, rc[0], rc[1], (0, 255, 0))
+			if record:
+				if initVideo and videoOutp:
+					if roiRect:
+						vw = roiRect[1][0] - roiRect[0][0]
+						vh = roiRect[1][1] - roiRect[0][1]
+					else:
+						vw = w
+						vh = h
+					print(' [initializing video as: ({}, {})]'.format(vw, vh))
+					vidout = cv2.VideoWriter(os.path.join(outdir, 'video-{:03}.{}'.format(frameCount+1, imgfmt)), fourcc, 1./tpf, (vw,vh))
+					initVideo = False
+				_, _, w0, h0 = cv2.getWindowImageRect(wTitle)
+				# img = cv2.resize(img, (w0, h0))
+				rfont = max(1, w / w0)
+				cv2.putText(img,'R',
+					(20, 20 + int(24 * rfont)),  # bottomLeftCornerOfText
+					cv2.FONT_HERSHEY_SIMPLEX,
+					rfont,  # Font size
+					(7, 7, 255),  # Color
+					1 + round(rfont)) # Line thickness, px
+			cv2.imshow(wTitle, img)
+		elif frameCount == nframes:
+			break
+		frameCount += 1
+		# Ensure that fps does not exceed teh required value
+		dt = time.perf_counter() - start
+		# print('dt: {}, tpf: {}'.format(dt, tpf))
+		if dt < tpf:
+			time.sleep(tpf - dt)
+		# Note: TIF images take 3.5x more space than PNG, but required much less CPU
+		if record:
+			await recordFrame(frame, outdir, frameCount, imgfmt)
+			print(' ', frameCount, end='', sep='', flush=True)
+	if vidout is not None:
+		vidout.release()
+	print()  # Ensure newline after the frames output
+
 
 if __name__ == '__main__':
 	parser = ArgumentParser(description='Document Taxonomy Builder.',
@@ -140,11 +313,53 @@ if __name__ == '__main__':
 	parser.add_argument('-n', '--no-gui-frames-number', dest='nframes', type=int, default=0,
 		# help='Caprute frames from the terminal without any Graphical User Interface')
 		help='The number of frames to be captured, being started without any Graphical User Interface')
-	parser.add_argument('-o', '--outp-dir', default='imgs',
+	parser.add_argument('-o', '--outp-dir', default='res',
 		help='Output directory for the captured images')
+	parser.add_argument('-c', '--camera', type=int, default=0,
+		help='The camera index to start the capturing')
+	parser.add_argument('-w', '--wnd-width', type=int, default=1600,
+		help='Initial wide of the GUI window in pixels')
+	parser.add_argument('-r', '--resolution', type=str, default=None,
+		help='Show ("x") or set ("<W>x<H>") camera resolution in the format: <WIDTH>x<HEIGHT>, e.g., 800x600')
 	args = parser.parse_args()
+	
+	camres = None   
+	if args.resolution:
+		assert 'x' in args.resolution, 'Invalid format of the argument'
+		if args.resolution != 'x':
+			camres = tuple(int(x) for x in args.resolution.split('x'))
+		else:
+			camres = tuple()
 
-	gentl = EGenTL()
-	grabber = EGrabber(gentl)
+	if not os.path.isdir(args.outp_dir):
+		os.makedirs(args.outp_dir)
 
-	run(grabber, args.nframes, args.fps, args.outp_dir, args.img_format)
+	try:
+		gentl = EGenTL()
+		grabber = EGrabber(gentl, 0, args.camera)  # EGrabber(gentl, card_ix, device_ix)
+		if camres:
+			print('Setting camera resolution: ', camres)
+			grabber.remote.set('Width', camres[0])
+			grabber.remote.set('Height', camres[1])
+		elif camres is not None:
+			print('Camera resolution: {}x{}'.format(grabber.remote.get('Width'), grabber.remote.get('Height')))
+			exit(0)
+		grabber.realloc_buffers(3)  # 3, 8
+		asyncio.run(loop(grabber, args.nframes, 1. / args.fps, args.outp_dir, args.img_format, args.wnd_width))
+	except generated.cEGrabber.GenTLException as err:
+		# Fallback to a standard web camera
+		vid = cv2.VideoCapture(0)
+		if camres:
+			print('Setting camera resolution: ', camres)
+			vid.set(cv2.CAP_PROP_FRAME_WIDTH, camres[0])
+			vid.set(cv2.CAP_PROP_FRAME_HEIGHT, camres[1])
+		elif camres is not None:
+			print('Camera resolution: {}x{}'.format(
+				int(vid.get(cv2.CAP_PROP_FRAME_WIDTH)), int(vid.get(cv2.CAP_PROP_FRAME_HEIGHT))))
+			exit(0)
+		vid.set(cv2.CAP_PROP_FPS, args.fps)
+		asyncio.run(wloop(vid, args.nframes, 1. / args.fps, args.outp_dir, args.img_format, args.wnd_width))
+		vid.release()
+
+	if not args.nframes:
+		cv2.destroyAllWindows()
